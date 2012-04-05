aa8f13e580c122e40f0fc9afcf9065944f462bf3
Honza Pokorny <me@honza.ca>
2012-04-05 18:29:48 -0300

    Add run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#/usr/bin/env bash
+
+export SPRUNGE_HOST='http://localhost:8080'
+export SPRUNGE_NAME='sprunge'
+export SPRUNGE_MONGO_URI=''
+export SPRUNGE_MONGO_DB_NAME=''
+
+python sprunge/app.py
